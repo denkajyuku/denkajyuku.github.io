991ac46b0a1cc7e5d1f5e7a9aab7a40a6aa102fb
川崎 彩香 <au0108@gmail.com>
2019-10-12 23:26:06 +0900

    ⚡️ パーセル仕様

--- style.6b54f5b7.js
+++ style.6b54f5b7.js
@@ -190,9 +190,9 @@ var reloadCSS = require('_css_loader');
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
 module.exports = {
-  "test": "_test_74b83",
-  "hoge": "_hoge_74b83",
-  "fuga": "_fuga_74b83"
+  "test": "_test_d94d0",
+  "hoge": "_hoge_d94d0",
+  "fuga": "_fuga_d94d0"
 };
 },{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
@@ -222,7 +222,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54913" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64856" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
