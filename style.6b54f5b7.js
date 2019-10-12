1cde1d862631d7f2521a636cbb31ae6f3a1342fa
Naoki Yamamoto <mtbk4919naoki@gmail.com>
2019-10-12 17:28:44 +0900

    test: gridautoplace

--- style.6b54f5b7.js
+++ style.6b54f5b7.js
@@ -191,8 +191,7 @@ module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
 module.exports = {
   "test": "_test_74b83",
-  "hoge": "_hoge_74b83",
-  "fuga": "_fuga_74b83"
+  "hoge": "_hoge_74b83"
 };
 },{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
@@ -222,7 +221,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54913" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65116" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
